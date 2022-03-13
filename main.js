c85f81971748fab28a050daa31c9267d1528864c
Talesagr <talesdd@outlook.com>
2022-03-13 16:42:06 -0300

    init

---
+++ main.js
@@ -0,0 +1,45 @@
+import '../Calculador-media/styles/base.css'
+
+const $button = document.querySelector("button");
+const input = document.querySelector("#numero");
+const $media = document.querySelector("#media")
+const $aprovado = document.querySelector("#aprovado")
+const $registros = document.querySelector("#registros")
+
+
+let total = 0;
+let qtdNotas = 0
+let media = 0
+let aprovado = "Não"
+
+function handleClick(event) {
+   event.preventDefault();
+
+const nota = Number(input.value);
+
+
+
+if (nota >= 0 && nota <= 10 && nota !== "") {
+total = total + nota;
+qtdNotas = qtdNotas + 1;
+
+
+media = total / qtdNotas
+
+if (media >= 7) {
+    aprovado = "Sim =)"
+} else {
+    aprovado = "Não =("
+}
+
+$media.textContent = media.toFixed(1);
+$aprovado.textContent = aprovado;
+
+$registros.insertAdjacentHTML("beforeend",  `<li>${nota}</li>`)
+} 
+
+
+
+
+}   
+$button.addEventListener("click", handleClick)
